050ddb16dca20ca80859a956a40c6c1281a1bc8f
John Hollingsworth <hollingsfsdf@gmail.com>
2020-05-03 14:14:23 -0600

    testing writing text

--- epd_2in13_V2_test.py
+++ epd_2in13_V2_test.py
@@ -28,9 +28,9 @@ try:
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     
     logging.info("1.Drawing on the image...")
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(image)
-    
+
     draw.rectangle([(0,0),(50,50)],outline = 0)
     draw.rectangle([(55,0),(100,50)],fill = 0)
     draw.line([(0,0),(50,50)], fill = 0,width = 1)
@@ -45,44 +45,36 @@ try:
     draw.text((110, 90), u'微雪电子', font = font24, fill = 0)
     epd.display(epd.getbuffer(image))
     time.sleep(2)
-    
-    # read bmp file 
+
+    # read bmp file
     logging.info("2.read bmp file...")
     image = Image.open(os.path.join(picdir, '2in13.bmp'))
     epd.display(epd.getbuffer(image))
     time.sleep(2)
-    
+
     # read bmp file on window
     logging.info("3.read bmp file on window...")
     # epd.Clear(0xFF)
     image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    image1.paste(bmp, (2,2))    
+    image1.paste(bmp, (2,2))
     epd.display(epd.getbuffer(image1))
     time.sleep(2)
-    
+
     # # partial update
     logging.info("4.show time...")
     time_image = Image.new('1', (epd.height, epd.width), 255)
-    time_draw = ImageDraw.Draw(time_image)
-    
+    text_draw = ImageDraw.Draw(time_image)
+
     epd.init(epd.FULL_UPDATE)
     epd.displayPartBaseImage(epd.getbuffer(time_image))
-    
+
     epd.init(epd.PART_UPDATE)
     num = 0
-    while (True):
-        time_draw.rectangle((120, 80, 220, 105), fill = 255)
-        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
-        epd.displayPartial(epd.getbuffer(time_image))
-        num = num + 1
-        if(num == 10):
-            break
-    
-    logging.info("Clear...")
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xFF)
-    
+    text_draw.rectangle((120, 80, 220, 105), fill = 255)
+    text_draw.text((120, 80), 'Hello there', font = font24, fill = 0)
+    epd.displayPartial(epd.getbuffer(time_image))
+
     logging.info("Goto Sleep...")
     epd.sleep()
         
